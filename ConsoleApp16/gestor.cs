da35f6af40ee875cd56dcf00e3fb3c3350d77cf1
eykel <antogar678@gmail.com>
2026-02-11 14:50:55 -0600

    first commit

---
+++ ConsoleApp16/gestor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System;
+using System.Collections.Generic;
+
+namespace GestionAlquilerVehiculos
+{
+    public class GestorVehiculos
+    {
+        private List<Vehiculo> listaVehiculos = new List<Vehiculo>();
+
+        public void AgregarVehiculo(Vehiculo vehiculo)
+        {
+            listaVehiculos.Add(vehiculo);
+        }
+
+        public void ListarVehiculos()
+        {
+            foreach (Vehiculo v in listaVehiculos)
+            {
+                v.MostrarInfo();
+                Console.WriteLine("-----------------");
+            }
+        }
+
+        public Vehiculo BuscarVehiculo(string placa)
+        {
+            foreach (Vehiculo v in listaVehiculos)
+            {
+                if (v.Placa == placa)
+                {
+                    return v;
+                }
+            }
+            return null;
+        }
+    }
+}
